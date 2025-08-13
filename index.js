efdecbe5c5a7fa2ee5b3285d343c2d75ddfb2947
Judith Camille Rosette <167699879+helicohedron@users.noreply.github.com.>
2025-08-14 02:06:26 +0800

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,131 @@
+import express from 'express';
+import process from 'node:process';
+import data from './data.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('port', PORT);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello World!'
+  });
+});
+
+// GET /words
+app.get('/words', (req, res) => {
+  //const title = 'Cybersecurity Word List'
+  // const html = `
+  //   <h1>Word List</h1>
+  //   <ul>
+  //     ${data.map(item => `<p><li>${item.word}</li></p>`).join('')}
+      
+  //   </ul>
+  // `;
+  // res.send(html);
+
+  res.status(200).json(
+    data.map(item => item.word)
+  );
+});
+
+// GET /words/:word
+app.get('/words/:word', (req, res) => {
+  const { word } = req.params;
+  const found = data.find(item => item.word === word.toLowerCase());
+  if (!found) {
+    res.status(404).send('Error 404: Not found.');
+  }
+
+  // const html = `
+  //   <h3>${found.word}</h3>
+  //   <p><i>${found.type}</i></p>
+  //   <p>${found.definition}</p>
+  // `
+  // res.send(html)
+
+  res.status(200).json({
+    word: found.word,
+    type: found.type,
+    definition: found.definition
+  });
+});
+
+// GET /filter?type=verb
+app.get('/filter', (req, res) => {
+  const { type } = req.query;
+  const filteredList = data.filter(item => 
+    item.type === type
+  );
+  
+  if (filteredList.length === 0) {
+    res.status(400).send('Error 400. Bad request.');
+  }
+
+  // const html = `
+  //   <h3>Words that are ${type}s</h3>
+  //   <ul>
+  //   ${filteredList.map(item => `<li>${item.word}</li>`).join('')}
+  //   </ul>
+  // `
+  // res.send(html);
+  res.status(200).json(
+    filteredList
+  );
+});
+
+// GET /search?q=ap
+app.get('/search', (req, res) => {
+  const { q } = req.query;
+  const searchList = data.filter(item => item.word.includes(q));
+
+  res.status(200).json(searchList);
+});
+
+// GET /random
+app.get('/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * (data.length -1));
+  const randomWord = data[randomIndex];
+
+  res.status(200).json(randomWord);
+});
+
+// GET /starts-with/:letter
+app.get('/starts-with/:letter', (req, res) => {
+  const { letter } = req.params;
+  const startsWith = data
+    .filter(item => item.word.startsWith(letter))
+    .map(item => item.word);
+
+  res.send(startsWith);
+});
+
+// GET /sort?by=length /sort?by=alphabet
+app.get('/sort', (req, res) => {
+  const { by } = req.query;
+  let sortedList = [];
+
+  switch (by) {
+    case 'alphabet':
+      sortedList = data
+        .map(item => item.word.toLowerCase())
+        .sort();
+        break;
+    case 'length':
+      sortedList = data
+        .map(item => item.word)
+        .sort((a,b) => 
+          a.length - b.length
+        );
+        break;
+  }
+
+  res.send(sortedList);
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`App is listening to port ${PORT}`);
+});
